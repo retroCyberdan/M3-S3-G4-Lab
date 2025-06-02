e1585dc053b86e640ca69b6f17c5ad94cdd78bae
retroCyberdan <105318240+retroCyberdan@users.noreply.github.com>
2025-06-02 11:50:46 +0200

    Add scripts + scene

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float _speed = 10f;
+    [SerializeField] private float _lifeSpan = 5f;
+
+    private void Start()
+    {
+        Destroy(gameObject, _lifeSpan); // <- distrugge il Proiettile dopo tot secondi che è stato generato
+    }
+
+    // Start is called before the first frame update
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.CompareTag("Enemy"))
+        {
+            Destroy(collision.gameObject); // <- distrugge il Nemico all'impatto
+            Destroy(gameObject); // <- distrugge il Proiettile all'impatto
+        }
+    }
+}
